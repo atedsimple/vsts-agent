2c9a281fdfc0e98d23201240ddf43dde99dd4af8
David Clifford <50109927+atedsimple@users.noreply.github.com>
2022-06-13 08:41:51 +0100

    Add files via upload

---
+++ scripts/Automation.ps1
@@ -0,0 +1,17 @@
+$AccountUrls = Get-AutomationVariable -Name 'AccountUrls'
+$PoolIds = Get-AutomationVariable -Name 'PoolIds'
+$Token = Get-AutomationVariable -Name 'Token'
+$PoolIds = $PoolIds -split ","
+
+$EncodedToken =[System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes(":$($Token)"))
+$Headers = @{authorization = "Basic $EncodedToken"}
+
+ForEach ($PoolId in $PoolIds) {
+	Write-Output "Pool ID: $PoolId"
+	$Agents = Invoke-RestMethod -uri "$($AccountUrls)/_apis/distributedtask/pools/$($PoolId)/agents?api-version=5.1" -Method Get -Headers $Headers
+	$Agents.value |
+		Where-Object { $_.status -eq 'offline' } |
+		ForEach-Object {
+			Invoke-RestMethod -uri "$($AccountUrls)/_apis/distributedtask/pools/$($PoolId)/agents/$($_.id)?api-version=5.1" -Method Delete -Headers $Headers
+		}
+}
